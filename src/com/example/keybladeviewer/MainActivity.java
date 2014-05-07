86868f6e0b23f34253f5525e5a0e061302190d72
BreshardB <breshard.busby@gmail.com>
2014-05-06 20:37:30 -0500

    Removed AsyncTask in MainActivity

--- src/com/example/keybladeviewer/MainActivity.java
+++ src/com/example/keybladeviewer/MainActivity.java
@@ -20,28 +20,12 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity{
-	
-	Keyblade[] keyblades;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Intent intent = getIntent();
-        Keyblade position = (Keyblade) intent.getSerializableExtra("Keyblade Clicked");
-        
-        TypedArray pics = getResources().obtainTypedArray(R.array.keyblade_pics);
-		Drawable[] drawables = new Drawable[pics.length()];	 
-
-		for (int i = 0; i < pics.length(); i++) {
-			drawables[i] = pics.getDrawable(i);
-		}
-		
-		pics.recycle();
-		
-		ImageView image = (ImageView)findViewById(R.id.image);
-		image.setImageDrawable(drawables[0]);
     }
     
     @Override
@@ -62,49 +46,5 @@ public class MainActivity extends ActionBarActivity{
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-    
-    private class DownloadKeybladeJSON extends AsyncTask<Integer, Void, Keyblade[]> {
-
-		private static final String EXTRA_MESSAGE = "keyblades";
-
-		@Override
-		protected Keyblade[] doInBackground(Integer... params) {
-			BufferedReader br;
-			InputStream is; 
-			try {
-				is = getResources().openRawResource(params[0].intValue());
-				br = new BufferedReader(new InputStreamReader(is));
-				String json = "";
-				String buffer = null;
-				while((buffer = br.readLine()) != null) {
-					json += buffer;
-				}
-				br.close();
-				
-				JSONObject keybladeJSON = new JSONObject(json);
-				JSONArray keybladeArray = (JSONArray)keybladeJSON.getJSONArray("keyblades");
-				keyblades = new Keyblade[keybladeArray.length()];
-				
-				for(int i = 0; i < keyblades.length; i++) {
-					keyblades[i] = new Keyblade();
-					JSONObject temp = (JSONObject)keybladeArray.get(i);
-					keyblades[i].name = temp.getString("name");
-					keyblades[i].strength = temp.getString("strength");
-					keyblades[i].ability = temp.getString("ability");
-					keyblades[i].magic = temp.getString("magic");
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-			
-			return keyblades;
-		}
-		
-		@Override
-		protected void onPostExecute(Keyblade[] result) {
-		}
     }
 }
